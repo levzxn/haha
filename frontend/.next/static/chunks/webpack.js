7004877a54e42715a752374d35137186394368f8
levi m. <106790460+levzxn@users.noreply.github.com>
2024-10-02 11:06:19 -0300

    kjkjkjk

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8da57757e8c1383"; }
+/******/ 		__webpack_require__.h = function() { return "2b0156abf9048f79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
