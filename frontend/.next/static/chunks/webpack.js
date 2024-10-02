8510fd153b1a7fe096ebebe1d8875ca3b8895e00
Lucas Franco <127855437+lucasfc-dev@users.noreply.github.com>
2024-10-02 10:58:33 -0300

    aaa

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7fb8d1dcbdd1b754"; }
+/******/ 		__webpack_require__.h = function() { return "3289b941f6683a9a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
